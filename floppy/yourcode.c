976e272386c5fd329991063b671b750a9a502a3e
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-02-21 05:25:30 -0500

    c

--- floppy/yourcode.c
+++ floppy/yourcode.c
@@ -50,42 +50,38 @@ makeAccessWork (char * dir1, char * dir2, char * pathname) {
 	/*
 	 * Implement your code here.
 	 */
-	struct stat stat_obj_dir1;
-	struct stat stat_obj_dir2;
-	struct stat stat_obj_file;
+	struct stat stat_dir1;
+	struct stat stat_dir2;
+	struct stat stat_file;
+	struct stat new_stat_dir1;
+	struct stat new_stat_dir2;
+	struct stat new_stat_file;
 
 
 	//remember old permissions, add read access to parent and grandparents
 	//by adding one more bit mask on
-	int r1 = stat(dir1, &stat_obj_dir1);
+	int r1 = stat(dir1, &stat_dir1);
 	output_permissions(stat_obj_dir1.st_mode);
 	chmod(dir1, stat_obj_dir1.st_mode | S_IROTH);
+	int n1 = stat(dir1, &new_stat_dir1);
+	output_permissions(new_stat_dir1.st_mode);
 
 
 	int r2 = stat(dir2, &stat_obj_dir2);
 	output_permissions(stat_obj_dir2.st_mode);
 	chmod(dir2, stat_obj_dir2.st_mode | S_IROTH);
+	int n2 = stat(dir2, &new_stat_dir2);
+	output_permissions(new_stat_dir2.st_mode);
 
 
 	int r3 = stat(pathname, &stat_obj_file);
 	output_permissions(stat_obj_file.st_mode);
 	mode_t new_file_mode = stat_obj_file.st_mode | S_IROTH | S_IWOTH;
 	chmod(pathname, new_file_mode);
+	int n3 = stat(dir2, &new_stat_file);
+	output_permissions(new_stat_file.st_mode);
 
 	
-	printf("\nafter\n");
-
-	struct stat new_stat_dir1;
-	struct stat new_stat_dir2;
-	struct stat new_stat_file;
-
-	int r4 = stat(dir1, &new_stat_dir1);
-	int r5 = stat(dir2, &new_stat_dir2);
-	int r6 = stat(pathname, &new_stat_file);
-
-	output_permissions(new_stat_dir1.st_mode);
-	output_permissions(new_stat_dir2.st_mode);
-	output_permissions(new_stat_file.st_mode);
 
 	printf("dir1=%s, dir2=%s, pathname=%s", dir1, dir2, pathname);
 
