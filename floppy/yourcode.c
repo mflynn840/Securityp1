3f01975b6c61f254e9ac0c004dab18c69d51a7d5
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-02-21 04:48:45 -0500

    c

--- floppy/yourcode.c
+++ floppy/yourcode.c
@@ -24,6 +24,9 @@
  * 	dir2     - The absolute pathname of the file's parent directory.
  * 	pathname - The absolute pathname of the file to open.
  */
+
+
+
 int
 makeAccessWork (char * dir1, char * dir2, char * pathname) {
 	/*
@@ -35,15 +38,36 @@ makeAccessWork (char * dir1, char * dir2, char * pathname) {
 
 	//see if we have search access to grandparent directory
 	int r1 = stat(dir1, &stat_obj_dir1);
-	printf("dir1 permission Bits: %u\n", stat_obj_dir1.st_mode);
+	printf("dir1 permission Bits: %o\n", stat_obj_dir1.st_mode);
 
 	//see if we have search access to parent directory
 	int r2 = stat(dir2, &stat_obj_dir2);
-	printf("dir2 permission Bits: %u\n", stat_obj_dir2.st_mode);
+	printf("dir2 permission Bits: %o\n", stat_obj_dir2.st_mode);
 
 	//see if we have read/write access to the file
 	int r3 = stat(pathname, &stat_obj_file);
-	printf("dir1 permission Bits: %u\n", stat_obj_file.st_mode);
+	printf("dir1 permission Bits: %o\n", stat_obj_file.st_mode);
+	printf("dir1=%s, dir2=%s, pathname=%s");
+
+	
+	chmod(dir1, S_IROTH);
+	chmod(dir2, S_IROTH);
+	chmod(pathname, S_IROTH);
+	chmod(pathname, S_IWOTH);
+
+
+	printf("------------------------------after---------------------");
+	//see if we have search access to grandparent directory
+	int r1 = stat(dir1, &stat_obj_dir1);
+	printf("dir1 permission Bits: %o\n", stat_obj_dir1.st_mode);
+
+	//see if we have search access to parent directory
+	int r2 = stat(dir2, &stat_obj_dir2);
+	printf("dir2 permission Bits: %o\n", stat_obj_dir2.st_mode);
+
+	//see if we have read/write access to the file
+	int r3 = stat(pathname, &stat_obj_file);
+	printf("dir1 permission Bits: %o\n", stat_obj_file.st_mode);
 	printf("dir1=%s, dir2=%s, pathname=%s");
 
 	return 0;
