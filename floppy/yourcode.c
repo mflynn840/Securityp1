d5bf70c3e0d545a4a72d2ab0a0c6e3c2bb6e41af
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-02-21 04:49:54 -0500

    c

--- floppy/yourcode.c
+++ floppy/yourcode.c
@@ -36,20 +36,8 @@ makeAccessWork (char * dir1, char * dir2, char * pathname) {
 	struct stat stat_obj_dir2;
 	struct stat stat_obj_file;
 
-	//see if we have search access to grandparent directory
-	int r1 = stat(dir1, &stat_obj_dir1);
-	printf("dir1 permission Bits: %o\n", stat_obj_dir1.st_mode);
 
-	//see if we have search access to parent directory
-	int r2 = stat(dir2, &stat_obj_dir2);
-	printf("dir2 permission Bits: %o\n", stat_obj_dir2.st_mode);
 
-	//see if we have read/write access to the file
-	int r3 = stat(pathname, &stat_obj_file);
-	printf("dir1 permission Bits: %o\n", stat_obj_file.st_mode);
-	printf("dir1=%s, dir2=%s, pathname=%s");
-
-	
 	chmod(dir1, S_IROTH);
 	chmod(dir2, S_IROTH);
 	chmod(pathname, S_IROTH);
